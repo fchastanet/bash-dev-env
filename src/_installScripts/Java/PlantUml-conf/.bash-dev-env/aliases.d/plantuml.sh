f277924ca359420c210999fdd7d77f2aab18f6a2
François Chastanet <fchastanet@gmail.com>
2025-07-15 18:55:30 +0200

    feat: Update BashTools and PlantUml scripts with new functionalities
    - Updated BashTools script to change the embedded configuration directory and its base64 string.
    - Added new Assert::fileExists function to check file existence and ownership.
    - Introduced Backup::file function to backup files before installation.
    - Enhanced Install::file and Install::structure functions for better file handling and ownership management.
    - Added logging capabilities for status messages in Log::displayStatus and Log::logStatus functions.
    - Updated PlantUml script to include new installation logic and copy structure functionality.
    - Created new aliases for PlantUml server start/stop commands and added a new aliases file for BashTools.

---
+++ src/_installScripts/Java/PlantUml-conf/.bash-dev-env/aliases.d/plantuml.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+alias plantumlServerStart='java -jar /opt/java/plantuml.jar -picoweb &'
+alias plantumlServerStop='pkill -f "java -jar /opt/java/plantuml.jar -picoweb"'
--- src/_installScripts/Java/PlantUml.sh
+++ src/_installScripts/Java/PlantUml.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# @embed "${BASH_DEV_ENV_ROOT_DIR}/src/_installScripts/Java/PlantUml-conf" as conf_dir
 
 plantUmlBeforeParseCallback() {
   Linux::requireJqCommand
@@ -13,7 +14,7 @@ dependencies() {
 }
 
 fortunes() {
-  echo -e "${__INFO_COLOR}$(scriptName)${__RESET_COLOR} -- run ${__HELP_EXAMPLE}nohup java -jar /opt/java/plantuml.jar -picoweb &${__RESET_COLOR}"
+  echo -e "${__INFO_COLOR}$(scriptName)${__RESET_COLOR} -- run alias ${__HELP_EXAMPLE}plantumlServerStart${__RESET_COLOR} or ${__HELP_EXAMPLE}nohup java -jar /opt/java/plantuml.jar -picoweb &${__RESET_COLOR}"
   echo -e "can be used as plantuml local web server for Vscode plantuml plugin to generate plantuml diagrams."
   echo "%"
   echo -e "${__INFO_COLOR}$(scriptName)${__RESET_COLOR} -- /opt/java/plantuml.jar now integrates elk and smetana layout engines"
@@ -40,6 +41,12 @@ plantumlVersionCallback() {
 }
 
 install() {
+  # shellcheck disable=SC2154
+  Conf::copyStructure \
+    "${embed_dir_conf_dir}" \
+    "$(fullScriptOverrideDir)" \
+    ".bash-dev-env"
+
   Linux::Apt::installIfNecessary --no-install-recommends \
     graphviz
 
@@ -55,4 +62,6 @@ testInstall() {
   # shellcheck source=/dev/null
   source "${HOME}/.sdkman/bin/sdkman-init.sh"
   Version::checkMinimal "plantumlVersionCallback" -version "1.2024.8" cat || return 1
+  Assert::fileExists "/opt/java/plantuml.jar" || return 1
+  Assert::fileExists "${HOME}/.bash-dev-env/aliases.d/plantuml.sh" || return 1
 }
